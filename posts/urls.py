344bd3e72ad88443ee127ac1d62f4ef67e5541db
Saidbek <saidbek156@gmail.com>
2024-07-06 17:07:12 +0500

    Comment Like funksiya va Post Update qiluvchi funksiya qoshdim

--- posts/urls.py
+++ posts/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path('create/', PostCreateAPIView.as_view(), name='create'),
     path('<int:pk>/comments/', PostCommentListView.as_view(), name='post-comments'),
     path('<int:pk>/comments/create/', PostCommentCreateAPIView.as_view(), name='comments-create'),
+
+    # Like Urls
     path('<int:pk>/like/', PostLikeAPIView.as_view(), name='like'),
+    path('comment/<int:pk>/like/', PostCommentLikeAPIView.as_view(), name='comment-like'),
+
+    # Post Update Urls
+    path('update/<int:pk>/', PostUpdateAPIView.as_view(), name='update')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from rest_framework import generics, permissions, status
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from posts.models import PostModel, CommentModel, PostLikeModel
+from posts.models import PostModel, CommentModel, PostLikeModel, CommentLikeModel
 from posts.serializers import PostSerializer, CommentSerializer
 from shared.custom_pagination import CustomPagination, CommentPagination
 
@@ -70,3 +69,62 @@ class PostLikeAPIView(APIView):
             }
 
             return Response(response, status=status.HTTP_200_OK)
+
+
+class PostCommentLikeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        comment_like = CommentLikeModel.objects.filter(comment_id=pk, user=self.request.user)
+        if comment_like.exists():
+            comment_like.delete()
+
+            response = {
+                'status': True,
+                'message': 'Successfully unliked'
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
+        else:
+            CommentLikeModel.objects.create(comment_id=pk, user=self.request.user)
+
+            response = {
+                'status': True,
+                'message': 'Successfully liked'
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
+
+class PostUpdateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def put(self, request, pk):
+        post = PostModel.objects.filter(pk=pk)
+        if not post.exists():
+            response = {
+                'status': False,
+                'message': 'Post does not exist'
+            }
+
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = PostSerializer(post.first(), data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+
+            response = {
+                'status': True,
+                'message': 'Successfully Updated'
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
+        else:
+
+            response = {
+                'status': False,
+                'message': 'Error updating post',
+                'errors': serializer.errors
+            }
+
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
